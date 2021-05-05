83d41289a7fb8120a2b0f0632ccb9b76e8dffcf6
kashturi-v <kashturi.vasanthan@gmail.com>
2021-05-05 11:18:50 -0400

    Created new files for the helper classes

---
+++ Image.java
@@ -0,0 +1,9 @@
+class Image{
+    String imageName;
+    String imageLocation;
+
+    public Image(String imageName, String imageLocation){
+        this.imageName = imageName;
+        this.imageLocation = imageLocation;
+    }
+}
---
+++ PQNode.java
@@ -0,0 +1,14 @@
+public class PQNode{
+    int priority;
+    Queue num;
+    PQNode next;
+    PQNode prev;
+
+    public PQNode(int priority, Queue num, PQNode prev, PQNode next){
+        this.priority = priority;
+        this.num = num;
+        this.prev = prev;
+        this.next = next;
+        
+    }
+}
