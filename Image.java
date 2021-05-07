86f9faae1911e2c0ce64804e9799810d299b34bd
Kashturi Vasanthan <35414011+kashturi-v@users.noreply.github.com>
2021-05-07 11:58:26 -0400

    Delete Image.java

--- Image.java
+++
@@ -1,9 +0,0 @@
-class Image{
-    String imageName;
-    String imageLocation;
-
-    public Image(String imageName, String imageLocation){
-        this.imageName = imageName;
-        this.imageLocation = imageLocation;
-    }
-}
